f1a11f4e496ada4c5c1c2a4311c62b31241a9cbe
MituBarman <147874527+MituBarman@users.noreply.github.com>
2024-06-11 18:12:21 +0530

    Delete bmenterprise/backend directory

--- bmenterprise/backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-const ConnectDB = require("./config/database");
-dotenv.config({ path: "./config/config.env" });
-
-ConnectDB();
-app.listen(process.env.PORT, () => {
-  console.log(process.env.PORT);
-});
